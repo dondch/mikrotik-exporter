911ce387c9a088d0208f3ffe1603d2df00c9c6c7
dondch <ddonatas@gmail.com>
2020-05-09 09:01:11 +0300

    change to 386

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ NAME=`basename ${DIR}`
 SHA=`git rev-parse --short HEAD`
 VERSION=${VERSION:-$SHA}
 
-GOOS=linux GOARCH=amd64 go build .
+GOOS=linux GOARCH=386 go build .
 
 docker build -t nshttpd/${NAME}:${VERSION} .
 docker push nshttpd/${NAME}:${VERSION}
